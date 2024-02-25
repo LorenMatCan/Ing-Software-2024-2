776fa086e1ab8b89559b0da8f7b200e405888b5b
LorenMatCan <saramatutec@ciencias.unam.mx>
2024-02-24 19:33:54 -0600

    Se modifica error de dedo

--- IngSoftLab.sql
+++ IngSoftLab.sql
@@ -18,7 +18,7 @@ CREATE TABLE `usuarios` (
   `superUser` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`idUsuario`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `peliculas` (
   `idPelicula` int NOT NULL AUTO_INCREMENT,
